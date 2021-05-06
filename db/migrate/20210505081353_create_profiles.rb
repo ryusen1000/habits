8d627337d9df45a5c68bae7324c68c7059963da8
USER <81149239+ryusen1000@users.noreply.github.com>
2021-05-06 16:45:10 +0900

    アクションテーブルの作成と記述修正

--- db/migrate/20210505081353_create_profiles.rb
+++ db/migrate/20210505081353_create_profiles.rb
@@ -1,9 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-      t.string :age,         nill: false
-      t.string :weight,      nill: false
-      t.references :user,    nill: false, foreign_key: true
+      t.string :age,         null: false
+      t.string :weight,      null: false
+      t.references :user,    null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20210506053503_create_actions.rb
+++ db/migrate/20210506053503_create_actions.rb
@@ -1,7 +1,11 @@
 class CreateActions < ActiveRecord::Migration[6.0]
   def change
     create_table :actions do |t|
-
+      t.text :act,           null: false
+      t.integer :amount,     null: false
+      t.text :if_act,        null: false
+      t.integer :if_amount,  null: false
+      t.references :task,    null: false, foreign_key: true
       t.timestamps
     end
   end
